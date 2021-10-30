b23faa183e78b8ef4cddc8ebbb56649ffb7b4741
Jash Palavwala <jashpalavwala39@gmail.com>
2021-10-30 15:28:03 +0530

    convention changed

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,6 @@
-import Header from "./Header";
-import RootNavigation from "./RootNavigation";
-import FolderView from "./FolderView";
-import TabBarCustomButton from "./TabBarCustomButton";
+/* eslint-disable prettier/prettier */
+import Header from './Header';
+import RootNavigation from './RootNavigation';
+import FolderView from './FolderView';
+import TabBarCustomButton from './TabBarCustomButton';
 export { Header, RootNavigation, FolderView, TabBarCustomButton };
